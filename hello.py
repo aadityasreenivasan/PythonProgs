883edff662204d09472e4f75cc734cd273228a40
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-05-16 00:27:37 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,2 @@
-print("hello world! This is Aaditya")
+print("hello world! This is Aaditya Sreenivasan")
+print("i am a new learner in this field and would like to hear advice and suggestions from all!:))")
