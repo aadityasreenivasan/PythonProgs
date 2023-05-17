8f35c68aedcce006fd688f97d591c75293f19b45
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-05-17 22:12:41 +0530

    Create matrixdiagSum.py

---
+++ matrixdiagSum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        l=len(mat)
+        if l==1: return mat[0][0]
+
+        sum=0
+        #iterating through array Diagonals
+        for i in range(l):
+            sum+=mat[i][i]
+            sum+=mat[i][-1-i]
+        
+        if l%2==1:
+            sum-=mat[l//2][l//2]
+        
+        return sum
