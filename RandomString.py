6d53c592c7e854ba91d50ae050592b4224d5a0a4
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-05-22 13:05:39 +0530

    Update RandomString.py

--- RandomString.py
+++ RandomString.py
@@ -1,1 +1,8 @@
 # program to generate the random string in upper and lower case letters.  
+import random  
+import string  
+def Upper_Lower_string(length): # define the function and pass the length as argument  
+    # Print the string in Lowercase  
+    result = ''.join((random.choice(string.ascii_lowercase) for x in range(length))) # run loop until the define length  
+    print(" Random string generated in Lowercase: ", result)  
+  
