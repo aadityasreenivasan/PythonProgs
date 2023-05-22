8871169de4dd61c4af6d247c7342637061f6ddbe
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-05-22 13:00:40 +0530

    Create transposeMatrix.py

---
+++ transposeMatrix.py
@@ -0,0 +1,9 @@
+# Define a matrix A  
+A = [[5, 4, 3],  
+     [2, 4, 6],  
+     [4, 7, 9],  
+     [8, 1, 3]]  
+# Define an empty matrix of reverse order  
+transResult = [[0, 0, 0, 0],    
+                [0, 0, 0, 0],  
+                [0, 0, 0, 0]]  
