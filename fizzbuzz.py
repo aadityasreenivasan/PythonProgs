fee045599fa0c32aa58ea0b21507a5b83437f020
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-05-25 16:11:18 +0530

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,10 @@
-def fizbuzz():
-    for fbz in range(50):
+def fizbuzz(n: int):
+    """
+    param: n- integer until fiuzzbuzz game will run
+    """
+    #taking numbers starting from 0 to n
+    for fbz in range(n+1):
+
         if fbz%3==0 and fbz%5==0:
             print("FizzBuzz")
             continue
