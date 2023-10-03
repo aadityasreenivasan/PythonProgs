34c3c3e52e26c5de901f08e564049adefafdfdbf
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-10-03 10:30:14 +0530

    Update Unique Number of Occurrences.py

--- Unique Number of Occurrences.py
+++ Unique Number of Occurrences.py
@@ -8,12 +8,12 @@ Output: true
 Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
 """
 
-class Solution:
-    def uniqueOccurrences(self, arr: List[int]) -> bool:
-        b=set(arr)
-        c=[]
 
-        for i in list(b):
-            c.append(arr.count(i))
+def uniqueOccurrences(self, arr) -> bool:
+    b=set(arr)
+    c=[]
+
+    for i in list(b):
+        c.append(arr.count(i))
         
-        return len(set(c))==len(c)
+    return len(set(c))==len(c)
